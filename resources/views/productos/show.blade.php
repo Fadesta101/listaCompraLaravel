a4e1e046c0b2c3c04ad6bb18fc3027dc576b647c
alumno <alumno@vm1>
2021-11-26 09:07:48 +0100

    Lista de Compra-parte-2

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -4,4 +4,17 @@
 
 Vista detalle producto {{ $id }}
 
+<div class="row">
+
+    <div class="col-sm-4">
+        <img src="https://picsum.photos/200/300/?random" style="height:200px"/>
+        {{-- TODO: Imagen genérica de los productos --}}
+
+    </div>
+    <div class="col-sm-8">
+        <p>Nombre: {{ $producto[0] }}</p>
+        <p>Categoría: {{ $producto[1] }}</p>
+
+    </div>
+</div>
 @stop
